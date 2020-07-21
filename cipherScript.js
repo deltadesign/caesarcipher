d1f7cd1e1a7469a07d0d62907d28e1f9e3ac4588
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2020-07-21 17:29:02 +0100

    2nd Commit

--- cipherScript.js
+++ cipherScript.js
@@ -1,5 +1,53 @@
-function encryptInput(input){
+const encode = () => {
+  let message = document.getElementById("userInput").value.toLowerCase().trim();
+  let resultArray = [];
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let num = Number(document.getElementById("num").value);
+ 
+  for ( i=0 ; i < message.length ; i++ ) {
+    let letter = message[i];
+    let letterRegEx = /[^a-z]/;
+    if (letterRegEx.test(letter)){
+      resultArray.push(letter)
+    }else{
+    let index = alphabet.indexOf(letter);
+    let newIndex = index + num;
+    if (newIndex < 0){
+      newIndex = (index + 26) + num;
+    } else if (newIndex > 25){
+      newIndex = (index - 26) + num;
+    }
+    resultArray.push(alphabet[newIndex]);
+    }
+  }
+  
+document.getElementById("result").innerHTML = resultArray.join('');
 
 }
 
-export {encryptInput};
+const decode = () => {
+  let message = document.getElementById("userInput").value.toLowerCase().trim();
+  let resultArray = [];
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let num = Number(document.getElementById("num").value);
+ 
+  for ( i=0 ; i < message.length ; i++ ) {
+    let letter = message[i];
+    let letterRegEx = /[^a-z]/;
+    if (letterRegEx.test(letter)){
+      resultArray.push(letter)
+    }else{
+    let index = alphabet.indexOf(letter);
+    let newIndex = index - num;
+    if (newIndex < 0){
+      newIndex = (index + 26) - num;
+    } else if (newIndex > 25){
+      newIndex = (index - 26) - num;
+    }
+    resultArray.push(alphabet[newIndex]);
+    }
+  }
+  
+document.getElementById("result").innerHTML = resultArray.join('');
+
+}
